afd530d8a04a280a825348343c8e39d4afe53582
Michael Alicea <mfalicea58@gmail.com>
2017-06-18 21:29:19 -0400

    Add the basic word model

---
+++ models/word.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WordSchema = new Schema({
+  word: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+module.exports = mongoose.Model('Word', WordSchema);
